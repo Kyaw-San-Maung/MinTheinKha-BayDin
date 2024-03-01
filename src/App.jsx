6786e5cb9cbda3b3adf6aa0bf1b37b93b78f46f5
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-03-02 02:13:39 +0630

    build-install react-query for fetching data from json server.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useQuery } from "react-query";
 import "./App.css";
 
 function App() {
+  useQuery({
+    queryKey: [""],
+  });
   return <h1 className="text-red-800 text-3xl font-bold ">Hello world!</h1>;
 }
 
---
+++ src/api/FetchQuestion.jsx
@@ -0,0 +1,6 @@
+export async function FetchQuestion() {
+  const response = await fetch("http://localhost:3000/questions").then((res) =>
+    res.json()
+  );
+  return response;
+}
