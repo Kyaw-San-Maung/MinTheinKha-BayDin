6657c0c35a5bb80be9c1e39e8dacfa980439778e
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2024-03-02 02:39:32 +0630

    feat-Finishing my data fetching feature from json-server for question part.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { useQuery } from "react-query";
 import "./App.css";
+import { FetchQuestion } from "./api/FetchQuestion";
 
 function App() {
-  useQuery({
-    queryKey: [""],
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["questions"],
+    queryFn: FetchQuestion,
   });
-  return <h1 className="text-red-800 text-3xl font-bold ">Hello world!</h1>;
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return JSON.stringify(`Error: ${error.message}`);
+
+  console.log("This is data :", data);
+  return (
+    <>
+      <h2 className="font-bold text-4xl justify-center flex">
+        Min Thein Kha - Bay Din Question List
+      </h2>
+      {data.map((question) => (
+        <div key={question.id} className="mx-52 my-4">
+          <div className="bg-blend-normal border rounded border-black justify-start">
+            <h2 className="py-3 mx-3">
+              {question.questionNo}.
+              <span className="mx-5">{question.questionName}</span>
+            </h2>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
--- src/api/FetchQuestion.jsx
+++ src/api/FetchQuestion.jsx
@@ -2,5 +2,6 @@ export async function FetchQuestion() {
   const response = await fetch("http://localhost:3000/questions").then((res) =>
     res.json()
   );
+  console.log("CAlling Json Server");
   return response;
 }
